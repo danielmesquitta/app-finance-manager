5e27f5456ec75a004ed6230477826fed8022e94d
Daniel Mesquita <danielmesquitta123@gmail.com>
2025-04-05 12:08:41 -0300

    fix: android navigation buttons overlapping with tabs

--- src/app/app/_layout.tsx
+++ src/app/app/_layout.tsx
@@ -1,102 +1,108 @@
 import {
-	IconChartBar,
-	IconChartBarActive,
-	IconCoins,
-	IconCoinsActive,
-	IconCreditCardPay,
-	IconCreditCardPayActive,
-	IconDotsCircleHorizontal,
-	IconDotsCircleHorizontalActive,
-	IconSmartHome,
-	IconSmartHomeActive,
+  IconChartBar,
+  IconChartBarActive,
+  IconCoins,
+  IconCoinsActive,
+  IconCreditCardPay,
+  IconCreditCardPayActive,
+  IconDotsCircleHorizontal,
+  IconDotsCircleHorizontalActive,
+  IconSmartHome,
+  IconSmartHomeActive,
 } from "@/assets/app";
 import { Text } from "@/components";
 import { colors } from "@/styles";
 import {
-	Tabs as ExpoRouterTabs,
-	TabList,
-	TabTrigger,
-	TabSlot,
-	type TabTriggerSlotProps,
+  Tabs as ExpoRouterTabs,
+  TabList,
+  TabSlot,
+  TabTrigger,
+  type TabTriggerSlotProps,
 } from "expo-router/ui";
-import { forwardRef, type Ref, type FC } from "react";
+import { forwardRef, type FC, type Ref } from "react";
 import { Pressable, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import type { SvgProps } from "react-native-svg";
 
 interface TabButtonProps extends TabTriggerSlotProps {
-	icon: FC<SvgProps>;
-	label: string;
-	activeIcon: FC<SvgProps>;
+  icon: FC<SvgProps>;
+  label: string;
+  activeIcon: FC<SvgProps>;
 }
 
 const TabButton = forwardRef(
-	(
-		{
-			icon: Icon,
-			label,
-			isFocused,
-			activeIcon: ActiveIcon,
-			...rest
-		}: TabButtonProps,
-		ref: Ref<View>,
-	) => {
-		return (
-			<Pressable {...rest} ref={ref}>
-				<View className="px-4 py-3 items-center gap-1">
-					{isFocused ? <ActiveIcon /> : <Icon color={colors.gray[400]} />}
-					<Text className="text-xs text-gray-400">{label}</Text>
-				</View>
-			</Pressable>
-		);
-	},
+  (
+    {
+      icon: Icon,
+      label,
+      isFocused,
+      activeIcon: ActiveIcon,
+      ...rest
+    }: TabButtonProps,
+    ref: Ref<View>
+  ) => {
+    return (
+      <Pressable {...rest} ref={ref}>
+        <View className="px-4 py-3 items-center gap-1">
+          {isFocused ? <ActiveIcon /> : <Icon color={colors.gray[400]} />}
+          <Text className="text-xs text-gray-400">{label}</Text>
+        </View>
+      </Pressable>
+    );
+  }
 );
 
 export default function Layout() {
-	return (
-		<ExpoRouterTabs>
-			<TabSlot />
+  const insets = useSafeAreaInsets();
 
-			<TabList className="bg-white pt-2 pb-4 px-6 rounded-t-lg">
-				<TabTrigger name="index" href="/app" asChild>
-					<TabButton
-						icon={IconSmartHome}
-						label="Início"
-						activeIcon={IconSmartHomeActive}
-					/>
-				</TabTrigger>
+  return (
+    <ExpoRouterTabs>
+      <TabSlot />
 
-				<TabTrigger name="transactions" href="/app/transactions" asChild>
-					<TabButton
-						icon={IconCreditCardPay}
-						label="Transações"
-						activeIcon={IconCreditCardPayActive}
-					/>
-				</TabTrigger>
+      <TabList
+        style={{ paddingBottom: insets.bottom * 0.8 }}
+        className="bg-white pt-2 pb-4 px-6 rounded-t-lg"
+      >
+        <TabTrigger name="index" href="/app" asChild>
+          <TabButton
+            icon={IconSmartHome}
+            label="Início"
+            activeIcon={IconSmartHomeActive}
+          />
+        </TabTrigger>
 
-				<TabTrigger name="investments" href="/app/investments" asChild>
-					<TabButton
-						icon={IconChartBar}
-						label="Invest."
-						activeIcon={IconChartBarActive}
-					/>
-				</TabTrigger>
+        <TabTrigger name="transactions" href="/app/transactions" asChild>
+          <TabButton
+            icon={IconCreditCardPay}
+            label="Transações"
+            activeIcon={IconCreditCardPayActive}
+          />
+        </TabTrigger>
 
-				<TabTrigger name="budget" href="/app/budget" asChild>
-					<TabButton
-						icon={IconCoins}
-						label="Orçam."
-						activeIcon={IconCoinsActive}
-					/>
-				</TabTrigger>
+        <TabTrigger name="investments" href="/app/investments" asChild>
+          <TabButton
+            icon={IconChartBar}
+            label="Invest."
+            activeIcon={IconChartBarActive}
+          />
+        </TabTrigger>
 
-				<TabTrigger name="more" href="/app/more" asChild>
-					<TabButton
-						icon={IconDotsCircleHorizontal}
-						label="Mais"
-						activeIcon={IconDotsCircleHorizontalActive}
-					/>
-				</TabTrigger>
-			</TabList>
-		</ExpoRouterTabs>
-	);
+        <TabTrigger name="budget" href="/app/budget" asChild>
+          <TabButton
+            icon={IconCoins}
+            label="Orçam."
+            activeIcon={IconCoinsActive}
+          />
+        </TabTrigger>
+
+        <TabTrigger name="more" href="/app/more" asChild>
+          <TabButton
+            icon={IconDotsCircleHorizontal}
+            label="Mais"
+            activeIcon={IconDotsCircleHorizontalActive}
+          />
+        </TabTrigger>
+      </TabList>
+    </ExpoRouterTabs>
+  );
 }
