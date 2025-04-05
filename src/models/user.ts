2726615082ba1419c1e2bc310ad5f6eff9b0f99d
Daniel Mesquita <danielmesquitta123@gmail.com>
2025-04-05 18:29:07 -0300

    feat: add has_budget to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,14 +1,15 @@
 export interface User {
-	id: string;
-	tier: string;
-	name: string;
-	email: string;
-	avatar: string;
-	provider: string;
-	updated_at: string;
-	created_at: string;
-	external_id: string;
-	verified_email: boolean;
-	synchronized_at: string;
-	subscription_expires_at: string;
+  id: string;
+  tier: string;
+  name: string;
+  email: string;
+  avatar: string;
+  provider: string;
+  updated_at: string;
+  created_at: string;
+  external_id: string;
+  verified_email: boolean;
+  synchronized_at: string;
+  subscription_expires_at: string;
+  has_budget: boolean;
 }
